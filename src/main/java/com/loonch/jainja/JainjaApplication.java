86a8bf585dbb16dafb1941637e8c8e656a2f69c2
gloonch <mahdihadian@hotmail.com>
2023-11-16 23:05:16 +0330

    init

---
+++ src/main/java/com/loonch/jainja/JainjaApplication.java
@@ -0,0 +1,13 @@
+package com.loonch.jainja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JainjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JainjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loonch/jainja/JainjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loonch.jainja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JainjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
